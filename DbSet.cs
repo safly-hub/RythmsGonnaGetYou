caffb4af36fe426688d6279ec34955ade6741743
LUCAS SAFLY <saflycorp@gmail.com>
2021-01-26 13:40:48 -0500

    Dbset internal class <T>

--- DbSet.cs
+++ DbSet.cs
@@ -1,6 +1,12 @@
+using System;
+
 namespace RhythmsGonnaGetYou
 {
     internal class DbSet<T>
     {
+        internal object Include(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
